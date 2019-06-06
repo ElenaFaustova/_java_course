d531679edbf838fced072520be19d373d808df28
helenfaust <helenfaust@bk.ru>
2019-06-06 22:17:03 +0200

    Реализована проверка и обеспечение предусловий для удаления и модификации групп.

--- addressbook-web-tests/src/test/java/HF/study/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/HF/study/addressbook/tests/GroupDeletionTests.java
@@ -1,5 +1,6 @@
 package HF.study.addressbook.tests;
 
+import HF.study.addressbook.model.GroupData;
 import org.testng.annotations.*;
 
 public class GroupDeletionTests extends TestBase {
@@ -7,6 +8,10 @@ public class GroupDeletionTests extends TestBase {
   @Test
   public void testGroupDeletion() throws Exception {
     app.getNavigationHelper().gotoGroupPage();
+    // проверяем наличие группы для удаления:
+    if (! app.getGroupHelper().isThereAGroup()) {
+      app.getGroupHelper().createGroup(new GroupData("test1", null, "test3"));
+    }
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().deleteSelectedGroups();
     app.getGroupHelper().returnToGroupPage();
