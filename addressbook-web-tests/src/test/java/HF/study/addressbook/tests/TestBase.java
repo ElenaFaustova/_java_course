278463bdf247080ee0dca052c0e04dce694808c4
helenfaust <helenfaust@bk.ru>
2019-06-30 22:01:19 +0200

    Задание №14: Вынесено описание тестового стенда в конфигурационный файл

--- addressbook-web-tests/src/test/java/HF/study/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/HF/study/addressbook/tests/TestBase.java
@@ -5,9 +5,10 @@ import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 
+
 public class TestBase {
 
-  protected static final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
+  protected static final ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.FIREFOX));
 
   @BeforeSuite(alwaysRun = true)
   public void setUp() throws Exception {
