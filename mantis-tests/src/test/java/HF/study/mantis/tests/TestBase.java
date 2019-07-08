ebbac7efb0b0eb65ae7b0991cdfb0424feb3bad2
helenfaust <helenfaust@bk.ru>
2019-07-08 21:20:08 +0200

    Задание №17: Создан проект для Mantis

---
+++ mantis-tests/src/test/java/HF/study/mantis/tests/TestBase.java
@@ -0,0 +1,27 @@
+package HF.study.mantis.tests;
+
+import HF.study.mantis.appmanager.ApplicationManager;
+import org.openqa.selenium.remote.BrowserType;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+
+public class TestBase {
+
+  protected static final ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.FIREFOX));
+
+  @BeforeSuite(alwaysRun = true)
+  public void setUp() throws Exception {
+    app.init();
+  }
+
+  @AfterSuite(alwaysRun = true)
+  public void tearDown() throws Exception {
+    app.stop();
+  }
+
+  public ApplicationManager getApp() {
+    return app;
+  }
+
+}
