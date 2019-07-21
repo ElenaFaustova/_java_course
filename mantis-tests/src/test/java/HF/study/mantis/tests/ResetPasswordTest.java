e641c7699c9554aa8416af81d251c5ebacb43fe3
helenfaust <helenfaust@bk.ru>
2019-07-21 15:27:25 +0200

    ResetPasswordTest

--- mantis-tests/src/test/java/HF/study/mantis/tests/ResetPasswordTest.java
+++ mantis-tests/src/test/java/HF/study/mantis/tests/ResetPasswordTest.java
@@ -1,12 +1,15 @@
 package HF.study.mantis.tests;
 
 import HF.study.mantis.model.MailMessage;
+import HF.study.mantis.model.UserData;
+import HF.study.mantis.model.Users;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.lanwen.verbalregex.VerbalExpression;
 
 import java.util.List;
+import java.util.Set;
 
 import static org.testng.Assert.assertTrue;
 
@@ -17,23 +20,26 @@ public class ResetPasswordTest extends TestBase {
     app.mail().start();
   }
 
+
   @Test
   public void testRegistration() throws Exception {
     skipIfNotFixed(0000001);
-    long now = System.currentTimeMillis();
-    String user = String.format("user%s", now);
+    Users users = app.db().users();
+    UserData user = app.db().users().iterator().next();
+    //UserData user = new UserData().withId(user1.getId()).withUsername(user1.getUsername()).withEmail(user1.getEmail());
+    int userId = user.getId();
+    String username = user.getUsername();
     String password = "password";
-    String email = String.format("user1%s@localhost.", now);
-    app.resetPassword().start("administrator", "root");
+
+    long now = System.currentTimeMillis();
+    app.resetPassword().start("administrator", "root", userId);
     List<MailMessage> mailMessages = app.mail().waitForMail(1, 10000);
-    String confirmationLink = findConfirmationLink(mailMessages, email);
+    String confirmationLink = findConfirmationLink(mailMessages);
     app.resetPassword().finish(confirmationLink, "password");
-    assertTrue(app.newSession().login(user, password));
+    assertTrue(app.newSession().login(username, password));
   }
 
-
-
-  private String findConfirmationLink(List<MailMessage> mailMessages, String email) {
+  private String findConfirmationLink(List<MailMessage> mailMessages) {
     MailMessage mailMessage = mailMessages.get(0);
     VerbalExpression regex = VerbalExpression.regex().find("http://").nonSpace().oneOrMore().build();
     return regex.getText(mailMessage.text);
